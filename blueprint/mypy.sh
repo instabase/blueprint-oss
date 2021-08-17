c806abd0bccdcb975ba1589968a4251da990f7b4
ddanco <ddanco@gmail.com>
2021-08-17 16:24:44 -0400

    Update mypy tests

--- blueprint/mypy.sh
+++ blueprint/mypy.sh
@@ -9,7 +9,9 @@ export MYPYPATH="$PYTHONPATH"
 
 python3 -m mypy -m bp
 python3 -m mypy py/bp/rules/*.py
-python3 -m mypy reference_extractions/*.py
+python3 -m mypy reference_extractions/paystubs/*.py
+python3 -m mypy reference_extractions/checks/*.py
+python3 -m mypy reference_extractions/bill_of_lading/*.py
 python3 -m mypy unit_tests/*.py
 python3 -m mypy integration_tests/*.py
 python3 -m mypy py/bp/cli/*.py
