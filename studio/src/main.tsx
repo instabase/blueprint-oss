fc26da022a5aaa710af3164fab8c827d900624ea
Andrey Mishchenko <mishchea@gmail.com>
2021-12-09 18:33:07 -0500

    Fix typo

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// These imports has to come first for CSS reasons.
+// These imports have to come first for CSS reasons.
 import 'studio/components/Theme.css';
 import 'studio/components/Studio.css';
 
